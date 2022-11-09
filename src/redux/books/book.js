ae1714dae19029de77a5826037d85be26438a8ff
RichardChileya <richardchileyawk@gmail.com>
2022-11-09 22:52:08 +0200

    Update redux book file

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,12 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
 
-const initialState = [];
+const initialState = [
+  { id: uuidv4(), title: 'Book 1', author: 'React' },
+  { id: uuidv4(), title: 'Book 2', author: 'Redux' },
+];
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD: return [...state, action.payload];
-    case REMOVE: return state.filter((f) => f.title !== action.payload);
+    case REMOVE: return state.filter((f) => f.id !== action.payload);
     default: return state;
   }
 };
