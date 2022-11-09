252485bcbd14203541eefa0a9db4f20146f6c243
RichardChileya <richardchileyawk@gmail.com>
2022-11-09 22:46:07 +0200

    Update Add file with react-redux

--- src/Components/AddBooks.js
+++ src/Components/AddBooks.js
@@ -1,14 +1,19 @@
-import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { add } from '../redux/books/book';
 
-const Add = (props) => {
-  const [inputBook, SetInput] = useState({
+const Add = () => {
+  const [inputBook, setInputText] = useState({
     title: '',
     author: '',
   });
 
+  const dispatch = useDispatch();
+  const getBook = (title, author) => ({ id: uuidv4(), title, author });
+
   const onChange = (e) => {
-    SetInput({
+    setInputText({
       ...inputBook,
       [e.target.name]: e.target.value,
     });
@@ -16,31 +21,40 @@ const Add = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { handleBooks } = props;
-    if (inputBook.title.trim() && inputBook.author()) {
-      handleBooks(inputBook.title, inputBook.author);
+
+    if (inputBook.title.trim() && inputBook.author.trim()) {
+      dispatch(add(getBook(inputBook.title, inputBook.author)));
+      setInputText({
+        title: '',
+        author: '',
+      });
     }
   };
 
   return (
     <div>
-      <h1>Add Books</h1>
+      <h2>ADD NEW BOOK</h2>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           className="input-text"
-          placeholder="Author"
+          placeholder="Book title"
+          value={inputBook.title}
+          name="title"
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Book author"
           value={inputBook.author}
           name="author"
           onChange={onChange}
         />
+        <button className="input-submit" type="submit">ADD BOOK</button>
       </form>
     </div>
   );
-};
-
-Add.propTypes = {
-  handleBooks: PropTypes.func.isRequired,
 };
 
 export default Add;
