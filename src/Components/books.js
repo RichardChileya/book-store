a8258c58b78e8fd73f5639cad1e899289332eaf8
RichardChileya <richardchileyawk@gmail.com>
2022-11-10 20:16:31 +0200

    Update redux  book file

--- src/Components/books.js
+++ src/Components/books.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import DisplayBooks from './DisplayBooks';
 import Add from './AddBooks';
+import { fetchBooks } from '../redux/books/book';
 
-const Books = () => (
-  <div>
-    <DisplayBooks />
-    <Add />
-  </div>
-);
+const Books = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+  return (
+    <div>
+      <DisplayBooks />
+      <Add />
+    </div>
+  );
+};
 
 export default Books;
