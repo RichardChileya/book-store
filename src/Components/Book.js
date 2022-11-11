f88c345d2739eb11fdce5e2577ad8e8e544ba279
RichardChileya <richardchileyawk@gmail.com>
2022-11-11 16:45:43 +0200

    Add styling elementors

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Progress } from 'antd';
 import { useDispatch } from 'react-redux';
 import { remove } from '../redux/books/book';
 
@@ -9,11 +10,31 @@ const Book = (props) => {
 
   return (
     <li className="books-list">
-      <div id="booklist-display">
-        <h3>{book.title}</h3>
-        <h3>{book.author}</h3>
+      <div className="left-block">
+        <p className="category">{book.category}</p>
+        <div id="booklist-display">
+          <h2>{book.title}</h2>
+          <p className="author">{book.author}</p>
+        </div>
+        <div className="buttons-div">
+          <button type="button" className="buttons">Comment</button>
+          <hr />
+          <button type="button" className="buttons" onClick={() => dispatch(remove(book.item_id))}>Remove</button>
+          <hr />
+          <button type="button" className="buttons">Edit</button>
+        </div>
       </div>
-      <button type="button" onClick={() => dispatch(remove(book.item_id))}>Delete</button>
+      <p className="progress-bar">
+        <Progress type="circle" percent={85} strokeColor="#307bbe" strokelinecap="round" width={60} fill-opacity="1" />
+        <span>Completed</span>
+      </p>
+      <hr className="dividor" />
+      <div className="right-block">
+        <h3 className="chapter">Current Chapter</h3>
+        <p className="chapter-info">Chapter 1 </p>
+        <button type="button" className="update-progress">UPDATE PROGRESS</button>
+      </div>
+
     </li>
 
   );
